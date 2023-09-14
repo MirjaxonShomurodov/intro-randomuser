9f5d2f5e4ae0d083a93982589845191db727828e
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-09-14 17:22:27 +0500

    user

--- user.py
+++ user.py
@@ -1,6 +1,8 @@
 import requests
 import json
-
+from pprint import pprint
+user_data = 'https://randomuser.me/api/'
+url = 'https://randomuser.me/api/'
 
 def get_user(user_data: dict) -> dict:
     '''get user from data
@@ -16,12 +18,19 @@ def get_user(user_data: dict) -> dict:
             'country': user country
         }
     '''
-    pass
-
+    m = {}
+    response = requests.get(user_data).json()
+    m["lastname"] = response["results"][0]["name"]["last"]
+    m["age"] = response["results"][0]["dob"]["age"]
+    m["country"] = response["results"][0]["location"]["country"]
+    m["firstname"] = response['results'][0]["name"]["first"]
+    return m
+ 
+# data = get_user(user_data)
+# pprint(data)
 
 def get_users(url: str, n: int) -> list:
     '''get n users from url
-    
     Args:
         url (str): api url
         n (int): number of users
@@ -29,7 +38,14 @@ def get_users(url: str, n: int) -> list:
     Returns:
         list: list of users. user from get_user()
     '''
-    pass
+    name = {}
+    for i in range(n):
+        get_users(url,1)
+        response = requests.get(url).json()
+        name["users"] = response["results"][0]["name"]["first"]
+        return name["users"]
+name = get_users(url,n=2)
+print(name)
     
 
 def write_users_to_file(file_path: str, n: int):
@@ -39,5 +55,5 @@ def write_users_to_file(file_path: str, n: int):
         url (str): api url
         n (int): number of users
     '''
-    pass
+    
 
